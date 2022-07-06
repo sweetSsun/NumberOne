fdf37acccb2d8a7f9d3d7e97aac732500ce09dae
hjh <gjwlgp9904@gmail.com>
2022-07-06 11:11:12 +0900

    session region update

--- NumberOne/src/main/java/com/NumberOne/service/MemberService.java
+++ NumberOne/src/main/java/com/NumberOne/service/MemberService.java
@@ -142,7 +142,6 @@ public class MemberService {
 				//로그인 성공
 				session.setAttribute("loginId", loginMember.getMid());
 				session.setAttribute("loginProfile", loginMember.getMprofile());
-
 				session.setAttribute("loginRegion", loginMember.getMregion());
 				
 				ra.addFlashAttribute("msg", "로그인 되었습니다.");
@@ -229,16 +228,17 @@ public class MemberService {
 		  System.out.println("MemberService.updateMyInfoMemberModify() 호출"); 
 		  String loginId = (String) session.getAttribute("loginId");
 		  System.out.println("로그인 된 아이디 : " + loginId);
-			
-		  member.setMid(loginId);
-		  
-
+		 
+		 member.setMid(loginId);
 
 		      //이미지 파일
 		      MultipartFile mfile = member.getMfile();
 		      
 		      //이미지의 파일명
 		      String mprofile = "";
+		      String loginProfile = (String) session.getAttribute("mprofile");
+
+		      System.out.println("원본프로필 : " + loginProfile);
 		      
 		      //이미지 파일 처리	      
 		      if(!mfile.isEmpty()) {
@@ -249,14 +249,16 @@ public class MemberService {
 		         mprofile = uuid.toString()+"_"+mfile.getOriginalFilename();
 
 		         mfile.transferTo(  new File(savePath, mprofile)   );
-		      } 
-		         
-		         System.out.println("mprofile : " + mprofile);
 		         member.setMprofile(mprofile);
-		        
-	      
-		         
-		         
+		         System.out.println("변경프로필 : " + mprofile);
+		      }else {
+		    	  if(loginProfile !=null) {
+		    		  member.setMprofile(loginProfile);  
+		    	  } else {
+		    		  member.setMprofile(mprofile);  
+		    	  }
+		      }
+
 		         System.out.println(member);
 		      
 		      
