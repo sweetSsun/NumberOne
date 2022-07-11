ae524d72cf41b7b2c75544069cc3e7d9782de4c0
carol <wildwolf.lee@gmail.com>
2022-07-11 16:51:53 +0900

    ResellView, ResellupDelete, ResellMOdify ing..

--- NumberOne/src/main/java/com/NumberOne/dto/ZzimDto.java
+++ NumberOne/src/main/java/com/NumberOne/dto/ZzimDto.java
@@ -7,7 +7,7 @@ public class ZzimDto {
 	
 	private String zzmid;		//찜한 사람
 	private String zzubcode;	//찜한 중고거래 글번호	
-	
+	private String zzim_Check;	//찜 여부 체크
 	//마이페이지 - 찜 목록 출력으로 추가
 	private String ubtitle;		//찜한 글 제목
 	private String mnickname;	//원본 글 작성자 닉네임
